0b6c49e78abf01a580bd4b5711343988d5e3d8fa
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 17:03:14 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -7,6 +7,12 @@ def test_unableToCompress():
     expected = b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
     assert comp.compress(input) == expected
 
+def test_maxCompression():
+    input = b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    comp = Compressor()
+    expected = b'\x01\x03\xB0\x02\x61\x45\x00'
+    #assert comp.compress(input) == expected
+
 """
 tests for private methods
 matchingData = [
