e0766698c0745479de69b8e6b8409e6accaa69ab
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 18:23:09 -0500

    Update decompressor.py

--- MS_OVBA_Compression/decompressor.py
+++ MS_OVBA_Compression/decompressor.py
@@ -4,24 +4,47 @@ class Decompressor:
 
     def __init__(self, endian = 'little'):
         self.endian = endian
-        self.uncompressedData = bytearray(b'')
+        self.uncompressedData = b''
         
         # The chunk after compression
-        self.compressedData = bytearray(b'')
+        self.compressedChunk = b''
 
-        #the size in bytes of the chunk after compression
+        #the size in bytes of the compressed chunk
         self.compressedChunkSize = 0
 
         # is the data compressed?
         self.compressed = 1
 
-    def setCompressedData(self, data):
-        """
-        set the Compressed data attribute
-        """
-        if len(data) != self.compressedChunkSize - 2:
-            raise Exception("Expecting " + str(self.compressedChunkSize - 2) + " bytes, but given " + str(len(data)) + ".")
-        self.compressedData = data
+    def decompress(self, compressedContainer):
+        if compressedContainer[0] != '\x01':
+            raise Exception("The container signature byte must be \\x01.")
+        chunks = compressedContainer[1:]
+        while len(chunks) > 0:
+            header = chunks[0:2]
+            compressed, length = self.unpackHeader(header)
+            cmpressedDataLength = length - 2
+            if len(chunks) < length:
+                raise Exception("Expecting " + str(length - 2) + " data bytes, but given " + str(len(chunks - 2)) + ".")
+            compressedChunk = chunks[2:compressedDataLength + 2]
+            chunks = chunks[length + 2:]
+            self.uncompressedData += self.decompressChunk(compressedChunk)
+
+    def unpackHeader(self, header):
+        length = len(compressedHeader)
+        if length != 2:
+            raise Exception("The header must be two bytes. Given " + str(length) + ".")
+        intHeader = int.from_bytes(compressedHeader, "little")
+        # data is compressed if the least significat bit is 0b1
+        compressed = (intHeader & 0x8000) >> 15
+
+        # the 12 most significant bits is three less than the chunk size
+        length = (intHeader & 0x0FFF) + 3
+        if not(self.compressed) and self.compressedChunkSize != 4096:
+            raise Exception("If uncompressed, chunk must be 4096 bytes.")
+        signature = (intHeader & 0x7000) >> 12
+        if signature != 3:
+            raise Exception("Chunk signature must be three. Value is " + str(signature) + ".")
+        return compressed, length
 
     def setCompressedHeader(self, compressedHeader):
         """
@@ -46,18 +69,7 @@ class Decompressor:
     def getCompressedChunkSize(self):
         return self.compressedChunkSize
 
-    def getCompressedChunk(self):  
-        return self.getCompressedChunkHeader() + self.compressedData
-
-    def getCompressedChunkHeader(self):
-        compressedChunkFlag = 1 if self.compressed else 0
-        intHeader = (self.compressed << 15) | 0x3000 | (self.compressedChunkSize - 3)
-        packSymbol = '<' if self.endian == 'little' else '>'
-        format = packSymbol + 'H'
-        return struct.pack(format, intHeader)
-
-    def decompress(self, data):
-        orig_data = data
+    def decompressChunk(self, compressedChunk):
         """
         Decompress a bytearray
 
@@ -65,27 +77,31 @@ class Decompressor:
         :return: bytes
         :rtype: bytes
         """
-        while len(data) > 0:
+        orig_data = data
+        uncompressedChunk = b''
+        while len(compressedChunk) > 0:
           #flag is one byte
-          flagToken = data.pop(0)
-          if len(data) == 0:
+          flagToken = data[0]
+          compressedChunk = compressedChunk[1:]
+          if len(compressedChunk) == 0:
               raise Exception("There must be at least one token in each TokenSequence.")
           flagMask = 1
           for i in range(8):
               flag = flagToken & flagMask
               flagMask = flagMask << 1
               if flag == 0:
-                  if len(data) > 0:
-                      self.uncompressedData.append(data.pop(0))
+                  if len(compressedChunk) > 0:
+                      uncompressedChunk += compressedChunk[0]
+                      compressedChunk = compressedChunk[1:]
               else:
-                  if len(data) < 2:
+                  if len(compressedChunk) < 2:
                       raise Exception("Copy Token does not exist. FlagToken was " + str(flagToken) + " and decompressed chunk is " + self.uncompressedData + '.')
-                  help = copyTokenHelp(len(self.uncompressedData))
+                  help = copyTokenHelp(len(uncompressedData))
                   copyToken = unpackCopyToken(struct.unpack("<H", data[:2])[0], help)  # Note this this will always be little endian.
-                  del data[:2]
+                  compressedChunk = compressedChunk[2:]
                   
                   for i in range(copyToken["length"]):
                       offset = copyToken["offset"]
-                      length = len(self.uncompressedData)
-                      self.uncompressedData.append(self.uncompressedData[-1 * offset])
+                      length = len(uncompressedData)
+                      uncompressedData += uncompressedData[-1 * offset]
         return self.uncompressedData
