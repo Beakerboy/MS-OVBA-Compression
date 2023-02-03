92c7987ec8ff991dc33eb63d48ce4c7e244cd147
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 16:51:39 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -3,7 +3,7 @@ from MS_OVBA_Compression.compressor import Compressor
 
 def test_unableToCompress():
     input = b'abcdefghijklmnopqrstuv.'
-    comp = Compressor(input)
+    comp = Compressor()
     expected = b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E'
     assert comp.compress(input) == expected
 
