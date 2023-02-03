c8e9bee5c861c967dbe065c18fc916f623d44097
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 11:58:03 -0500

    Create test_helpers.py

---
+++ tests/test_helpers.py
@@ -0,0 +1,15 @@
+import pytest
+import MS_OVBA_Compression.helpers
+
+ceilLog2Data = [
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4),
+    (9, 4),
+    (17, 5),
+    (50, 6),
+]
+@pytest.mark.paramatrize("input, expected", ceilLog2Data)
+def test_cielLog2(input, expected):
+    assert ceilLog2(input) == expected
