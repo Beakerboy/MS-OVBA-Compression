251e4f457ae484ab0a6c21ed296af1261d7e5643
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 12:38:38 -0500

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -17,6 +17,7 @@ def test_cielLog2(input, expected):
 packCopyTokenData = [
     ([3, 8, 10], 0x7000),
     ([4, 8, 18], 0x3801),
+    ([4, 5, 25], 0x3000),
 ]
 
 @pytest.mark.parametrize("inputs, expected", packCopyTokenData)
