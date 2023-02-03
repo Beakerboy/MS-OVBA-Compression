985359d60d94a36c90d7a548adb7c609c2de37cf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 11:41:10 -0500

    Update compressor.py

--- MS_OVBA_Compression/compressor.py
+++ MS_OVBA_Compression/compressor.py
@@ -1,4 +1,6 @@
 import struct
+import MS_OVBA_Compression.helpers
+
 class Compressor:
     def __init__(self, endian='little'):
         self.endian = endian
@@ -113,39 +115,7 @@ class Compressor:
             maximumLength = help["maxLength"]
             length = min(maximumLength, bestLength)
             offset = len(self.activeChunk) - len(uncompressedStream) - bestCandidate
-            copyToken = self.packCopyToken(length, offset, help)
+            copyToken = packCopyToken(length, offset, help)
         else:
             copyToken = bytes(uncompressedStream[0])
         return copyToken, length
-
-    def copytokenHelp(self, difference):
-        """
-        Calculate a lengthMask, offsetMask, and bitCount from the length of the uncompressedData
-        """
-        bitCount = self.ceilLog2(difference)
-        lengthMask = 0xFFFF >> bitCount
-        offsetMask = ~lengthMask & 0xFFFF
-        maxLength = 0xFFFF << bitCount + 3
-        return {
-            "lengthMask": lengthMask,
-            "offsetMask": offsetMask,
-            "bitCount": bitCount,
-            "maxLength": maxLength
-        }
-
-    def packCopyToken(self, length, offset, help):
-        """
-        Create the copy token from the length, offset, and currect position
-        
-        return bytes
-        """
-        temp1 = offset - 1
-        temp2 = 16 - help["bitCount"]
-        temp3 = length - 3
-        return struct.pack("<H", (temp1 << temp2) | temp3)
-
-    def ceilLog2(self, int):
-        i = 4
-        while 2 ** i < int:
-            i += 1
-        return i
