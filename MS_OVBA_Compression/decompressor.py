e3836379f49d973bbb871f88e597b59f7580503c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 19:49:28 -0500

    Update decompressor.py

--- MS_OVBA_Compression/decompressor.py
+++ MS_OVBA_Compression/decompressor.py
@@ -72,7 +72,7 @@ class Decompressor:
                         raise Exception("Copy Token does not exist. FlagToken was " + str(flagToken) + " and decompressed chunk is " + self.uncompressedData + '.')
                     help = copyTokenHelp(len(uncompressedChunk))
                     # The copy Token is always packed into the compressed chuck little endian
-                    copyToken = struct.unpack("<H", data[:2])
+                    copyToken = int.from_bytes(data[:2], "little")
                     copyTokenData = unpackCopyToken(copyToken, help)
                     compressedChunk = compressedChunk[2:]
                     offset = copyTokenData["offset"]
