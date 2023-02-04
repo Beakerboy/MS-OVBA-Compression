38175493f6ce4cac4f281f774b45ec7a52e8509a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-03 20:31:57 -0500

    Update test_decompressor.py

--- tests/test_decompressor.py
+++ tests/test_decompressor.py
@@ -26,6 +26,16 @@ def test_containerTooShort():
     with pytest.raises(Exception) as e_info:
         result = comp.decompress(compressed)
 
+def test_containerTooLong():
+    """
+    The container must have a signature byte
+    """
+    comp = Decompressor()
+    compressed = b'\x01\x2F\xB0\x00\x23\x61\x61\x61\x62\x63\x64\x65\x82\x66\x00\x70\x61\x67\x68\x69\x6A\x01\x38\x08\x61\x6B\x6C\x00\x30\x6D\x6E\x6F\x70\x06\x71\x02\x70\x04\x10\x72\x73\x74\x75\x76\x10\x77\x78\x79\x7A\x00\3C\x3F\xFF'
+    with pytest.raises(Exception) as e_info:
+        result = comp.decompress(compressed)
+
+
 def test_badRawChunkHeader():
     """
     Raw chinks must have 4096 bytes of data. Header = 0x3FFF
