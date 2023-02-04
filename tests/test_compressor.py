5bb104d1ee4d59aa6b868a2641aa8f40e1dab3b7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-04 01:20:31 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -5,13 +5,13 @@ from ms_ovba_compression.compressor import Compressor
 def test_unableToCompress():
     input = b'abcdefghijklmnopqrstuv.'
     comp = Compressor()
-    expected = b('\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A'
-                 '\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
+    expected = (b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A'
+                + b'\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
     assert comp.compress(input) == expected
 
 def test_maxCompression():
-    input = b('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-              'aaaaaaaaa')
+    input = (b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+             + b'aaaaaaaaa')
     comp = Compressor()
     expected = b'\x01\x03\xB0\x02\x61\x45\x00'
     assert comp.compress(input) == expected
