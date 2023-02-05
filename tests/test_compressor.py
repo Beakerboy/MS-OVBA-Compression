55a59ed4af5fe9fcea6d99a4e87979414dbd5b04
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 10:08:26 -0500

    Update test_compressor.py

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -1,20 +1,20 @@
-from ms_ovba_compression.compressor import Compressor
+from ms_ovba_compression.ms_ovba import MsOvba
 
 
 def test_unableToCompress():
     input = b'abcdefghijklmnopqrstuv.'
-    comp = Compressor()
+    ms_ovba = MsOvba()
     expected = (b'\x01\x19\xB0\x00\x61\x62\x63\x64\x65\x66\x67\x68\x00\x69\x6A'
                 + b'\x6B\x6C\x6D\x6E\x6F\x70\x00\x71\x72\x73\x74\x75\x76\x2E')
-    assert comp.compress(input) == expected
+    assert ms_ovba.compress(input) == expected
 
 
 def test_maxCompression():
     input = (b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
              + b'aaaaaaaaaa')
-    comp = Compressor()
+    ms_ovba = MsOvba()
     expected = b'\x01\x03\xB0\x02\x61\x45\x00'
-    assert comp.compress(input) == expected
+    assert ms_ovba.compress(input) == expected
 
 
 def test_longPoorCompression():
