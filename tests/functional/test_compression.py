57e3ee0996f0e64131130aa886f90aa1c13e7e59
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-05 10:39:31 -0500

    Update test_compression.py

--- tests/functional/test_compression.py
+++ tests/functional/test_compression.py
@@ -3,6 +3,6 @@ from ms_ovba_compression.ms_ovba import MsOvba
 
 def test_normalCompresson():
     input = b'#aaabcdefaaaaghijaaaaaklaaamnopqaaaaaaaaaaaarstuvwxyzaaa'
-    ms_ovba = MsOVBA()
+    ms_ovba = MsOvba()
     result = ms_ovba.compress(input)
     assert ms_ovba.decompress(result) == input
