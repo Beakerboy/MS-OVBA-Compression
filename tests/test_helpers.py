e434393016841c01357de996128b96efb3fd18f4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-07 09:28:56 -0500

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,7 +2,7 @@ import pytest
 from ms_ovba_compression.ms_ovba import MsOvba
 
 
-ceilLog2Data = [
+ceil_log2_data = [
     (1, 4),
     (2, 4),
     (3, 4),
@@ -13,8 +13,8 @@ ceilLog2Data = [
 ]
 
 
-@pytest.mark.parametrize("input, expected", ceilLog2Data)
-def test_cielLog2(input, expected):
+@pytest.mark.parametrize("input, expected", ceil_log2_data)
+def test_ciel_log2(input, expected):
     assert MsOvba.ceilLog2(input) == expected
 
 
@@ -34,29 +34,29 @@ def test_packCopyToken(inputs, expected):
     assert MsOvba.packCopyToken(inputs[0], inputs[1], help) == expected
 
 
-copytokenHelpData = [
+copytoken_help_data = [
     (9, [4, 0x0FFF, 0xF000]),
     (17, [5, 0x07FF, 0xF800]),
 ]
 
 
-@pytest.mark.parametrize("input, expected", copytokenHelpData)
-def test_copytokenHelp(input, expected):
+@pytest.mark.parametrize("input, expected", copytoken_help_data)
+def test_copytoken_help(input, expected):
     result = MsOvba.copyTokenHelp(input)
     assert result["bitCount"] == expected[0]
     assert result["lengthMask"] == expected[1]
     assert result["offsetMask"] == expected[2]
 
 
-unpackCopyTokenData = [
+unpack_copytoken_data = [
     ([9, 0x7000], [3, 8]),
     ([32, 0x7002], [5, 15]),
     ([54, 0x3C00], [3, 16]),
 ]
 
 
-@pytest.mark.parametrize("inputs, expected", unpackCopyTokenData)
-def test_unpackCopyToken(inputs, expected):
+@pytest.mark.parametrize("inputs, expected", unpack_copytoken_data)
+def test_unpack_copytoken(inputs, expected):
     help = MsOvba.copyTokenHelp(inputs[0])
     result = MsOvba.unpackCopyToken(inputs[1], help)
     assert result["length"] == expected[0]
